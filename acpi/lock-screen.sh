68db4ea145eb16f8c89ef3b690ba585ea1c6fb71
Timur Batyrshin <erthad@gmail.com>
2013-12-06 17:18:28 +0400

    discover and export DISPLAY in lock-screen

--- acpi/lock-screen.sh
+++ acpi/lock-screen.sh
@@ -2,5 +2,6 @@
 
 for X11DISPLAY in /run/user/*/X11-display; do 
   USERNAME="$(stat -c %U $(dirname "${X11DISPLAY}"))"
+  export DISPLAY=":$(readlink "${X11DISPLAY}" | sed 's,/tmp/.X11-unix/X,,')"
   sudo -u "${USERNAME}" xdg-screensaver lock
 done
